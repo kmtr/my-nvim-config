1b3b57b07989de70f0c4204ba56ed915c3f4d52a
kmtr <kmtr@outlook.com>
2024-10-26 09:03:36 +0900

    Update: rust configration

--- lua/301-rust.lua
+++ lua/301-rust.lua
@@ -1,16 +1,18 @@
 -- :MasonInstall rust-analyzer codelldb
--- https://github.com/simrat39/rust-tools.nvim
+-- :MasonUpdate
+local lspconfig = require'lspconfig'
 
-local rt = require("rust-tools")
+local on_attach = function(client)
+end
 
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            checkOnSave = {
+                command = "clippy",
+            },
+        }
+    }
 })
 
